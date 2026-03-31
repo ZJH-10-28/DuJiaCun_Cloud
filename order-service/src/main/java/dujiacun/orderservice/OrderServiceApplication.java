ad87dc6743193a17473a620c92d82abc99a0d585
展金昊 <2251498846@qq.com>
2026-03-31 21:15:08 +0800

    update

--- order-service/src/main/java/dujiacun/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/dujiacun/orderservice/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package dujiacun.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ public class OrderServiceApplication {
 
     //发HTTP请求的工具
     @Bean
+    @LoadBalanced // 开启负载均衡
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- order-service/src/main/java/dujiacun/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/dujiacun/orderservice/service/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package dujiacun.orderservice.service;
 
+import dujiacun.orderservice.entity.UserEntity;
 import dujiacun.orderservice.entity.bo.OrderInfoBo;
 import dujiacun.orderservice.entity.bo.OrderParamBo;
 import dujiacun.orderservice.entity.dto.OrderResponseDto;
@@ -7,6 +8,7 @@ import dujiacun.orderservice.mapper.OrderMapper;
 import dujiacun.orderservice.util.BeanConvertUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderServiceImpl implements IOrderService {
@@ -14,8 +16,17 @@ public class OrderServiceImpl implements IOrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @Override
     public OrderInfoBo getOrderInfo(OrderParamBo orderParamBo) {
-        return BeanConvertUtil.convert(orderMapper.getOrderInfo(orderParamBo), OrderInfoBo.class);
+
+        OrderInfoBo orderInfoBo = BeanConvertUtil.convert(orderMapper.getOrderInfo(orderParamBo),OrderInfoBo.class);
+
+        String url = "http://user-service/user/" + orderInfoBo.getUserId();
+
+        orderInfoBo.setUserEntity(restTemplate.getForObject(url, UserEntity.class));
+        return orderInfoBo;
     }
 }

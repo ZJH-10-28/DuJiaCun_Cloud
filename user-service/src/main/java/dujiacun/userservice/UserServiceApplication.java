0752324c3905d22c129c714308e3fc5d92b77387
展金昊 <2251498846@qq.com>
2026-04-01 11:57:56 +0800

    启动Nacos服务

--- user-service/src/main/java/dujiacun/userservice/UserServiceApplication.java
+++ user-service/src/main/java/dujiacun/userservice/UserServiceApplication.java
@@ -2,6 +2,8 @@ package dujiacun.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,4 +12,9 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    //发HTTP请求的工具
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
